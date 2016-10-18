6b06b0b4d80cb438d7d25f54fa2ae19c13d1ee46
Cordian Alexander Daniluk <thecorluk@gmail.com>
2016-10-18 02:35:38 +0200

    Changed name of macro

--- boot.h
+++ boot.h
@@ -4,5 +4,5 @@ define(`BOOT_TEXT', `0x7c00')
 define(`BOOT_SIZE', `0x200')
 
 define(`BOOTSYS_TEXT', `(0x90000 - BOOTSYS_LEN)')
-define(`BOOTSYS_LEN', `0x10000')
+define(`BOOTSYS_SIZE', `0x10000')
 ')
