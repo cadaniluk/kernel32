c79c9f55bba491bb899f573e8cb70fef992feaff
Cordian Alexander Daniluk <thecorluk@gmail.com>
2016-11-02 21:23:43 +0000

    Added memory detection functionality
    The memory detection so far only uses ah=0xe820, int=0x15 and even that
    is still very basic, but at least we got something implemented now. I'm still
    unsure about how we're going to utilize the memory detection, but there is
    a use case for it for sure.
    Still many TODOs there, so work on those and finish the implementation!

---
+++ boot/include/mm_detect.h
@@ -0,0 +1,22 @@
+ifdef(`MM_DETECT_H',,
+
+
+define(`E820_SMAP', `0x534d4150') /* "SMAP" in ASCII */
+
+define(`E820_AVAILABLE', `0x1')
+define(`E820_RESERVED', `0x2')
+/* TODO: when youve implemented acpi (like, in 500 years) add functionality
+ * for acpi reclaim memory and acpi nvs memory. */
+define(`E820_ACPI_RECLAIM', `0x3')
+define(`E820_ACPI_NVS', `0x4')
+
+
+define(`E820_MAX_ENTRIES', `0x20')
+
+define(`E820_BASE_ADDR_LEN', `0x8')
+define(`E820_LEN_LEN', `0x8')
+define(`E820_TYPE_LEN', `0x4')
+define(`E820_ENTRY_LEN', `((E820_BASE_ADDR_LEN + E820_LEN_LEN + dnl
+E820_TYPE_LEN) * E820_MAX_ENTRIES)')
+
+) /* MM_DETECT_H */
