08fdff7c3bcebc91cb0dcb367a587251e7252a16
Cordian A. Daniluk <th3c0r1uk@gmail.com>
2016-12-15 21:51:16 +0100

    Add `SBP_` prefix to parameters to the secondary bootloader
    Add a `SBP_` prefix to the parameters to the secondary bootloader
    in order to prevent name collisions. We don't have something like
    namespaces so far (but maybe we'll have them one day), so such
    prefixes must do.

--- boot/include/sec_boot_params.h
+++ boot/include/sec_boot_params.h
@@ -2,7 +2,7 @@ ifdef(`SEC_BOOT_PARAMS_H',,
 define(`SEC_BOOT_PARAMS_H')
 
 define(`SBP_BASE', `0x500')
-define(`DRV_NUM', `SBP_BASE')
-define(`CURR_LINE', `(SBP_BASE + 0x1)')
+define(`SBP_DRV_NUM', `SBP_BASE')
+define(`SBP_CURR_LINE', `(SBP_BASE + 0x1)')
 
 )
