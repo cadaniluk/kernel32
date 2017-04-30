90bb4983c7a28bc7cb54cfd810681be81a72cc93
Cordian A. Daniluk <th3c0r1uk@gmail.com>
2017-05-01 00:21:03 +0200

    Add I/O address to parameter list
    The parameter interface between primary and secondary bootloader contains
    the I/O address of the VGA CRTC now.
    I also did some small things, which are not worth mentioning. I can't really
    remember what they were anyway.

--- include/sec_boot_params.h
+++ include/sec_boot_params.h
@@ -4,5 +4,6 @@ define(`SEC_BOOT_PARAMS_H')
 define(`SBP_BASE', `0x500')
 define(`SBP_DRV_NUM', `SBP_BASE')
 define(`SBP_CURR_LINE', `(SBP_BASE + 0x1)')
+define(`SBP_CRTC_ADDR', `SBP_BASE + 0x2')
 
 )
