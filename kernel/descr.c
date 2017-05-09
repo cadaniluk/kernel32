9019141b754f4dd5b2627fc1653c38e576b0b552
Cordian Alexander Daniluk <th3c0r1uk@gmail.com>
2017-05-09 14:00:54 +0000

    Improve IDT initialization and add ISRs
    There are proper ISRs added into the IDT now. They are just
    stubs for now, but I'm just getting started. There are some
    improvements regarding readability and organization.

--- kernel/descr.c
+++ kernel/descr.c
@@ -1,4 +1,5 @@
 include(`descr.h')
+include(`i8259A.h')
 
 descr_t gdt[GDT_SIZE] = {
 	0x0, /* null descriptor */
@@ -56,16 +57,27 @@ descr_t idt[IDT_SIZE];
 /* TODO: place in a section which is overridden by other data later on.
  * this is only needed for initialization and can be removed afterwards.
  */
-extern uint32_t addrs[];
+extern uint32_t addrs1[], addrs2[], addrs3[], addrs4[];
 __asm__(
 	".data\t\n"
-	"addrs:\t\n"
-	".long ex_div0, ex_debug, ex_breakpoint, ex_overflow, ex_beyond_range, "
-	"ex_bad_opcode, ex_no_fpu, ex_double_fault, ex_fpu_seg_overrun,"
-	"ex_bad_tss, ex_seg_not_present, ex_ss_fault, ex_general_prot,"
-	"ex_page_fault, ex_fpu_float_error, ex_align_check, ex_machine_check,"
-	"ex_simd_float, ex_virtual"
+	"addrs1:\t\n"
+	".long ex_div0, ex_debug\t\n"
+	"addrs2:\t\n"
+	".long ex_breakpoint, ex_overflow, ex_beyond_range, ex_bad_opcode,"
+	"ex_no_fpu, ex_double_fault, ex_fpu_seg_overrun, ex_bad_tss,"
+	"ex_seg_not_present, ex_ss_fault, ex_general_prot, ex_page_fault\t\n"
+	"addrs3:\t\n"
+	".long ex_fpu_float_error, ex_align_check, ex_machine_check,"
+	"ex_simd_float, ex_virtual\t\n"
+	"addrs4:\t\n"
+	".long irq0, irq1, irq2, irq3, irq4, irq5, irq6, irq7, irq8, irq9, irq10,"
+	"irq11, irq12, irq13, irq14, irq15"
 );
+
+define(`ADDR_COUNT1', `2')
+define(`ADDR_COUNT2', `12')
+define(`ADDR_COUNT3', `5')
+define(`ADDR_COUNT4', `16')
 
 /*
  * A dummy ISR for all user-defined interrupts that do not have their own
@@ -79,10 +91,6 @@ __asm__(
 	"isr_dummy_addr: .long isr_dummy"
 );
 
-define(`DESCR_COUNT1', `2')
-define(`DESCR_COUNT2', `12')
-define(`DESCR_COUNT3', `5')
-
 /*
  * Storing the addresses in an array and initializing the IDT in a loop
  * should be faster and smaller on disk than initializing every single
@@ -93,20 +101,23 @@ void idt_init(void)
 	/* TODO: replace unsigned int with size_t. */
 	unsigned int i = 0, j = 0;
 
-	for ( ; i < DESCR_COUNT1; ++i, ++j)
-		idt[i] = MK_INTG_DESCR(addrs[j], C0_SEL, GATE_32, 0);
+	for ( ; i < ADDR_COUNT1; ++i, ++j)
+		idt[i] = MK_INTG_DESCR(addrs1[j], C0_SEL, GATE_32, 0);
 	
 	/* Skip vector 0x2 (NMI). */
-	for (++i; i < DESCR_COUNT2; ++i, ++j)
-		idt[i] = MK_INTG_DESCR(addrs[j], C0_SEL, GATE_32, 0);
+
+	for (++i; i < ADDR_COUNT2; ++i, ++j)
+		idt[i] = MK_INTG_DESCR(addrs2[j], C0_SEL, GATE_32, 0);
 	
 	/* Skip vector 0xe (reserved). */
-	for (++i; i < DESCR_COUNT3; ++i, ++j)
-		idt[i] = MK_INTG_DESCR(addrs[j], C0_SEL, GATE_32, 0);
+
+	for (++i; i < ADDR_COUNT3; ++i, ++j)
+		idt[i] = MK_INTG_DESCR(addrs3[j], C0_SEL, GATE_32, 0);
 	
-/* TODO: add isr initialization. */
+	for (i = USER_INT_START; i < ADDR_COUNT4; ++i, ++j)
+		idt[i] = MK_INTG_DESCR(addrs4[j], C0_SEL, GATE_32, 0);
 
 	/* Start at user-defined interrupts. */
-	for (i = 0x20; i < IDT_SIZE; ++i)
+	for ( ; i < IDT_SIZE; ++i)
 		idt[i] = MK_INTG_DESCR(isr_dummy_addr, C0_SEL, GATE_32, 0);
 }
