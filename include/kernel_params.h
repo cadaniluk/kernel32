0bf25b21dda8328dedff63080f86c2913d5c0ca3
Cordian A. Daniluk <th3c0r1uk@gmail.com>
2017-05-13 21:31:20 +0200

    Change start of kernel parameters
    Since the secondary bootloader will also use the BIOS until the
    disk driver is finished, it cannot override the IVT, so the kernel
    needs to be loaded at a higher address. We don't override the
    BDA either.
    The kernel parameters length macro was a little buggy too.

--- include/kernel_params.h
+++ include/kernel_params.h
@@ -3,9 +3,11 @@ define(`KERNEL_PARAMS_H')
 
 include(`kernel.m4')
 
-define(`KP_BASE', `0x0')
+/* TODO: change to 0x0 as soon as we have all drivers ready and are BIOS-
+ * independent. */
+define(`KP_BASE', `0x500')
 define(`KP_E820_MAP', `KP_BASE')
 
-define(`KP_SIZE', `KERNEL_ORG')
+define(`KP_SIZE', `(KERNEL_ORG - KP_BASE)')
 
 ')
