981f53e742afc5375c67ae6200f3cfd5a92ed457
Cordian A. Daniluk <th3c0r1uk@gmail.com>
2017-07-16 01:54:16 +0200

    Add macro to create page dir entries for 4-MiB pages
    There is a new macro to create page directory entries to map 4-MiB
    pages into memory using PSE. Besides, I made little improvements
    here and there such as surrounding macro arguments with braces,
    adding TODOs (it's arguable if that is an "improvement"), and some
    other things. See for yourself, they should not require exhaustive
    documentation.

--- include/mm/paging.h
+++ include/mm/paging.h
@@ -1,8 +1,8 @@
 #ifndef PAGING_H
 #define PAGING_H
 
-#define PD_SIZE 4096
-#define PT_SIZE 4096
+#define PD_LEN 1024
+#define PT_LEN 1024
 #define PAGE_SIZE 4096
 
 /*
@@ -12,25 +12,49 @@
  * `pwt' - page-level write-through
  */
 #define MK_CR3(addr, pcd, pwt) ( \
-	(addr << 12) | \
-	(pcd << 4) | \
-	(pwt << 3) \
+	((addr) << 12) | \
+	((pcd) << 4) | \
+	((pwt) << 3) \
 )
 
 /*
- * 32-bit paging page directory entry (PDE) pointing to a PTE.
+ * 32-bit paging page directory entry (PDE) referencing a page table.
  * `addr' - physical address of 4-KiB aligned page table
  * `pcd' - page-level cache disable 
  * `pwt' - page-level write-through
  * `us' - (User/Supervisor) if user access is allowed
  * `rw' - if writing to the page is allowed
  */
-#define MK_PDE_TO_PTE(addr, pcd, pwt, us, rw) ( \
-	(addr << 12) | \
-	(pcd << 4) | \
-	(pwt << 3) | \
-	(us << 2) | \
-	(rw << 1) | \
+#define MK_PDE_TO_PT(addr, pcd, pwt, us, rw) ( \
+	((addr) << 12) | \
+	((pcd) << 4) | \
+	((pwt) << 3) | \
+	((us) << 2) | \
+	((rw) << 1) | \
+	0x1 \
+)
+
+
+/*
+ * 32-bit paging page directory entry (PDE) mapping a 4-MiB page.
+ * `addr' - physical address of 4-MiB page
+ * TODO: add second address part as soon as PAE is supported!
+ * `pat' - if Page Attribute Table is supported (TODO: is this correct?)
+ * `global' - whether page is global
+ * `pcd' - page-level cache disable 
+ * `pwt' - page-level write-through
+ * `us' - (User/Supervisor) if user access is allowed
+ * `rw' - if writing to the page is allowed
+ */
+#define MK_PDE_TO_PG(addr, global, pat, pcd, pwt, us, rw) ( \
+	((addr) << 22) | \
+	((pat) << 12) | \
+	((global) << 8) | \
+	(0x1 << 7) | \
+	((pcd) << 4) | \
+	((pwt) << 3) | \
+	((us) << 2) | \
+	((rw) << 1) | \
 	0x1 \
 )
 
@@ -40,27 +64,32 @@
 #define PDE_PWT 0x8
 #define PDE_PCD 0x10
 #define PDE_ACC 0x20
+#define PDE_DIRTY 0x40
 #define PDE_PS 0x80
-#define PDE_ADDR_SHIFT 12
+#define PDE_GLOBAL 0x100
+#define PDE_PAT 0x1000
+
+#define PDE_PT_ADDR_SHIFT 12
+#define PDE_PG_ADDR_SHIFT 22
 
 /*
  * 32-bit paging page table entry (PTE) for a 4-KiB page.
  * `addr' - physical address of 4-KiB page frame
  * `global' - whether page is global
- * `pat' - if Page Attribute Table is supported
+ * `pat' - if Page Attribute Table is supported (TODO: is this correct?)
  * `pcd' - page-level cache disable
  * `pwt' - page-level write-through
  * `us' - (User/Supervisor) if user access is allowed
  * `rw' - if writing to the page is allowed
  */
 #define MK_PTE(addr, global, pat, pcd, pwt, us, rw) ( \
-	(addr << 12) \
-	(global << 8) \
-	(pat << 7) \
-	(pcd << 4) \
-	(pwt << 3) \
-	(us << 2) \
-	(rw << 1) \
+	((addr) << 12) \
+	((global) << 8) \
+	((pat) << 7) \
+	((pcd) << 4) \
+	((pwt) << 3) \
+	((us) << 2) \
+	((rw) << 1) \
 	0x1 \
 )
 
@@ -74,8 +103,6 @@
 #define PTE_DIRTY 0x40
 #define PTE_GLOBAL 0x100
 #define PTE_ADDR_SHIFT 12
-
-#define KPG_DIR 0x100000
 
 #ifdef __C__
 
@@ -92,10 +119,8 @@ typedef uint32_t pde_t;
 /* A page table entry for plain 32-bit paging. */
 typedef uint32_t pte_t;
 
-/* TODO: mark this, along with certain other functions, as only needed for
- * initialization, so it can be overwritten later.
- */
-void setup_paging(void);
+typedef pde_t pg_dir_t[PD_LEN]
+__attribute__ ((aligned (sizeof(pde_t) * PD_LEN)));
 
 #endif
 
