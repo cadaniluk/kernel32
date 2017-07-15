5526db4c7464a19e12d8e643f73f94809e5b4ced
Cordian A. Daniluk <th3c0r1uk@gmail.com>
2017-07-15 16:53:07 +0200

    Add some macros

--- include/mm/paging.h
+++ include/mm/paging.h
@@ -1,27 +1,9 @@
 #ifndef PAGING_H
 #define PAGING_H
 
-#ifdef __C__
-
-#include <basetypes.h>
-
-/*
- * We're going to start slowly: 32-bit paging, no PAE or PSE. Future patches
- * will change that, though, depending on the exact processor in use.
- */
-
-/* A page directory entry for plain 32-bit paging. */
-typedef uint32_t pde_t;
-
-/* A page table entry for plain 32-bit paging. */
-typedef uint32_t pte_t;
-
-/* TODO: mark this, along with certain other functions, as only needed for
- * initialization, so it can be overwritten later.
- */
-void setup_paging(void);
-
-#endif
+#define PD_SIZE 4096
+#define PT_SIZE 4096
+#define PAGE_SIZE 4096
 
 /*
  * CR3 for 32-bit paging.
@@ -52,6 +34,15 @@ void setup_paging(void);
 	0x1 \
 )
 
+#define PDE_PRESENT 0x1
+#define PDE_RW 0x2
+#define PDE_US 0x4
+#define PDE_PWT 0x8
+#define PDE_PCD 0x10
+#define PDE_ACC 0x20
+#define PDE_PS 0x80
+#define PDE_ADDR_SHIFT 12
+
 /*
  * 32-bit paging page table entry (PTE) for a 4-KiB page.
  * `addr' - physical address of 4-KiB page frame
@@ -73,9 +64,39 @@ void setup_paging(void);
 	0x1 \
 )
 
+#define PTE_PRESENT 0x1
+#define PTE_RW 0x2
+#define PTE_US 0x4
+#define PTE_PWT 0x8
+#define PTE_PCD 0x10
 #define PTE_ACC 0x20
+#define PTE_PAT 0x80
 #define PTE_DIRTY 0x40
+#define PTE_GLOBAL 0x100
+#define PTE_ADDR_SHIFT 12
 
 #define KPG_DIR 0x100000
+
+#ifdef __C__
+
+#include <basetypes.h>
+
+/*
+ * We're going to start slowly: 32-bit paging, no PAE or PSE. Future patches
+ * will change that, though, depending on the exact processor in use.
+ */
+
+/* A page directory entry for plain 32-bit paging. */
+typedef uint32_t pde_t;
+
+/* A page table entry for plain 32-bit paging. */
+typedef uint32_t pte_t;
+
+/* TODO: mark this, along with certain other functions, as only needed for
+ * initialization, so it can be overwritten later.
+ */
+void setup_paging(void);
+
+#endif
 
 #endif
