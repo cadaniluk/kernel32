f99f86aacfd27d7622d80252c9cade7656a42129
Cordian A. Daniluk <th3c0r1uk@gmail.com>
2017-07-16 00:24:25 +0200

    Add flags header file
    This header file contains the flags in the flags register. Maybe
    I'll add wrappers that fetch and load flags values later.

---
+++ include/flags.h
@@ -0,0 +1,22 @@
+#ifndef __FLAGS_H
+#define __FLAGS_H
+
+#define FLAGS_CF 0x1
+#define FLAGS_PF 0x4
+#define FLAGS_AF 0x10
+#define FLAGS_ZF 0x40
+#define FLAGS_SF 0x80
+#define FLAGS_TF 0x100
+#define FLAGS_IF 0x200
+#define FLAGS_DF 0x400
+#define FLAGS_OF 0x800
+#define FLAGS_IOPL 0x3000
+#define FLAGS_NT 0x4000
+#define FLAGS_RF 0x10000
+#define FLAGS_VM 0x20000
+#define FLAGS_AC 0x40000
+#define FLAGS_VIF 0x80000
+#define FLAGS_VIP 0x100000
+#define FLAGS_ID 0x200000
+
+#endif
